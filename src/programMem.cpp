3b74edf378759c33b71df988cd4626f15ca3e349
Dillon Huff <dillonhuff@gmail.com>
2017-10-10 12:10:31 -0700

    Filling out programMem definition

--- src/programMem.cpp
+++ src/programMem.cpp
@@ -1,5 +1,28 @@
 #include "programMem.h"
 
+#include "utils.h"
+
 using namespace CoreIR;
+
 void addProgramMem(CoreIR::Context* c, CoreIR::Namespace* global) {
+  Params programMemParams({{"pcWidth", AINT}});
+
+  TypeGen* memParamsTypeGen =
+    global->newTypeGen("programMemTypeGen",
+		       programMemParams,
+		       [](Context* c, Args args) {
+			 //uint pcWidth = args.at("pcWidth")->get<int>();
+			 return c->Record();
+		       }
+		       );
+
+  Generator* programMem =
+    global->newGeneratorDecl("programMem",
+			     memParamsTypeGen,
+			     programMemParams);
+
+  programMem->setGeneratorDefFromFun([](ModuleDef* def, Context* c, Type* t, Args args) {
+      //uint pcWidth = args.at("pcWidth")->get<int>();
+      
+    });
 }
