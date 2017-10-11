e4dc1d4cdc059cb7edd56b0c14d64b9a8eb79461
Dillon Huff <dillonhuff@gmail.com>
2017-10-11 15:22:52 -0700

    Creating outputs for this programMem module

--- src/programMem.cpp
+++ src/programMem.cpp
@@ -11,12 +11,17 @@ void addProgramMem(CoreIR::Context* c, CoreIR::Namespace* global) {
     global->newTypeGen("programMemTypeGen",
 		       programMemParams,
 		       [](Context* c, Values args) {
-			 //uint pcWidth = args.at("pcWidth")->get<int>();
+			 uint pcWidth = args.at("pcWidth")->get<int>();
 			 uint numStages = args.at("numStages")->get<int>();
 			 uint stageBits = bitsNeededToStore(numStages);
 			 return c->Record({
 			     {"clk", c->Named("coreir.clkIn")},
-			       {"stageNumber", c->Array(stageBits, c->BitIn())}
+			       {"stageNumber", c->Array(stageBits, c->BitIn())},
+
+			       // Outputs
+				 {"opName", c->Array(pcWidth, c->Bit())},
+				   {"arg0", c->Array(pcWidth, c->Bit())},
+				     {"arg1", c->Array(pcWidth, c->Bit())}
 			   });
 		       }
 		       );
@@ -64,15 +69,19 @@ void addProgramMem(CoreIR::Context* c, CoreIR::Namespace* global) {
 			   {"depth", Const::make(c, pow(2, pcWidth))}});
 
       // Connect clock to sequential elements
+      def->connect("self.clk", "progMem.clk");
+      def->connect("self.clk", "PC.clk");
+      def->connect("self.clk", "opName.clk");
+      def->connect("self.clk", "arg0.clk");
+      def->connect("self.clk", "arg1.clk");
 
-      // Connect stage counter where needed
+      // Connect stage counter to enables where needed
+      // Set register enables, this is the same as stage connection?
 
       // Connect memory rdata to registers
       def->connect("progMem.rdata", "opCode.in");
       def->connect("progMem.rdata", "arg0.in");
       def->connect("progMem.rdata", "arg1.in");
-
-      // Set register enables
 
       // Connect PC to increments
       def->connect("pc.out", "inc1.in");
@@ -81,8 +90,12 @@ void addProgramMem(CoreIR::Context* c, CoreIR::Namespace* global) {
       def->connect("pc.out", "memMux.in.0");
       def->connect("inc1.out", "memMux.in.1");
       def->connect("inc2.out", "memMux.in.2");
-      
 
+      // Connect results to outputs
+      def->connect("opName.out", "self.opName");
+      def->connect("arg0.out", "self.arg0");
+      def->connect("arg1.out", "self.arg1");
+      
     });
 
   // Q: Can you pipeline a design just by merging some stage numbers in
